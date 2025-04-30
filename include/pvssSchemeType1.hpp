321e833a93382a6055e1cef856d74889fe149703
Sajjad Nasirzadeh <sajjadnasirzadeh@gmail.com>
2025-05-01 00:32:11 +0330

    Add new pvss scheme

---
+++ include/pvssSchemeType1.hpp
@@ -0,0 +1,116 @@
+#ifndef PVSSSCHEMETYPE1_HPP
+#define PVSSSCHEMETYPE1_HPP
+/* pvssSchemeType1.hpp
+ *
+ * Copyright (C) 2025
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject
+ * to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+ * OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ **/
+
+#include <vector>
+
+#include "NTL/mat_ZZ_pE.h"
+
+namespace NewPVSSScheme::PVSSType1 {
+    struct Params {
+        long numberOfParties;
+        long threshold;
+        NTL::ZZ p;
+        NTL::ZZ q;
+        long k;
+        NTL::ZZ bound;
+        NTL::ZZ_pE a;
+        NTL::vec_ZZ_pE v;
+        NTL::vec_ZZ_pE h;
+        NTL::mat_ZZ_pE A;
+        NTL::vec_ZZ_pE t;
+        NTL::Mat<NTL::vec_ZZ_pE> u;
+    };
+
+    struct PrivateKey {
+        NTL::ZZ_pE a;
+        NTL::ZZ_pE s;
+    };
+
+    struct PublicKey {
+        NTL::ZZ_pE a;
+        NTL::ZZ_pE b;
+    };
+
+    struct KeyProof {
+    };
+
+    struct KeyPair {
+        PublicKey publicKey;
+        PrivateKey privateKey;
+        KeyProof proof;
+    };
+
+    struct Cipher {
+        NTL::ZZ_pE u;
+        NTL::ZZ_pE v;
+    };
+
+    struct Commitment {
+        NTL::ZZ_pE c;
+    };
+
+    struct OpeningProof {
+        NTL::vec_ZZ output;
+    };
+
+    struct DistributionProof {
+        std::vector<Cipher> encryptedShares;
+        Commitment commitment;
+        OpeningProof proof;
+    };
+
+    struct DecryptionProof {
+        NTL::ZZ decryptedShare;
+    };
+
+    void _generateTrapdoor(NTL::mat_ZZ_pE &A, NTL::mat_ZZ_pE &trapdoor, long n, long m,
+                           const NTL::ZZ &q, NTL::ZZ_pX f, const NTL::ZZ &bound);
+
+    void _preSample(NTL::vec_ZZ_pE &x, const NTL::mat_ZZ_pE &trapdoor, const NTL::mat_ZZ_pE &A, const NTL::vec_ZZ_pE &b,
+                    const NTL::ZZ &q, NTL::ZZ_pX f, const NTL::ZZ &bound);
+
+    Params setup(long securityParameter, long numberOfParties, long threshold);
+
+    KeyPair generateKey(const Params &params, long index);
+
+    bool verifyKey(const Params &params, const PublicKey &publicKey, const KeyProof &proof);
+
+    DistributionProof distribute(const Params &params, const std::vector<PublicKey &> &publicKeys,
+                                 const NTL::ZZ &secret);
+
+    bool verifyDistribution(const Params &params, const std::vector<PublicKey &> &publicKeys,
+                            const DistributionProof &proof);
+
+    DecryptionProof decryptShare(const Params &params, const PublicKey &publicKey,
+                                 const PrivateKey &privateKey, const Cipher &encryptedShare);
+
+    bool verifyDecryption(const Params &params, const PublicKey &publicKey,
+                          const Cipher &encryptedShare, const DecryptionProof &proof);
+
+    NTL::ZZ reconstruct(const Params &params, const std::vector<NTL::ZZ> &decryptedShares);
+}
+
+#endif //PVSSSCHEMETYPE1_HPP
---
+++ src/NewPVSSScheme/pvssSchemeType1.cpp
@@ -0,0 +1,105 @@
+/* pvssSchemeType1.cpp
+*
+ * Copyright (C) 2025
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject
+ * to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+ * OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ **/
+
+#include "pvssSchemeType1.hpp"
+
+#include <NTL/mat_lzz_p.h>
+#include <NTL/xdouble.h>
+
+using namespace std;
+
+namespace NewPVSSScheme::PVSSType1 {
+    Params setup(const long securityParameter, const long numberOfParties, const long threshold) {
+        Params params;
+        params.numberOfParties = numberOfParties;
+        params.threshold = threshold;
+
+        RandomPrime(params.p, securityParameter);
+        RandomPrime(params.q, securityParameter * 4 + 4);
+
+        params.k = securityParameter;
+        power2(params.bound, params.k / 2 - 3);
+
+        NTL::ZZ_p::init(params.q);
+
+        NTL::ZZ_pX f;
+        SetCoeff(f, params.k);
+        SetCoeff(f, 0);
+        NTL::ZZ_pE::init(f);
+
+        random(params.a);
+
+        const long w = params.threshold + 1 + 4 * params.numberOfParties;
+        const long o = 3 * params.numberOfParties;
+        random(params.v, w);
+
+        long n = 2;
+        long m = 2 * n * (securityParameter * 4 + 4);
+        NTL::mat_ZZ_pE td;
+        _generateTrapdoor(params.A, td, n, m, params.q, f, params.bound);
+
+        NTL::vec_ZZ_pE tmp;
+        random(tmp, w);
+        params.t = params.A * tmp;
+
+        params.u.SetDims(w, w);
+        for (long i = 0; i < w; i++) {
+            for (long j = 0; j < w; j++) {
+                if (i == j) continue;
+
+                _preSample(params.u[i][j], td, params.A, params.v[i] / params.v[j] * params.t, params.q, f,
+                           params.bound);
+            }
+        }
+
+        NTL::ZZ_p::init(params.p);
+        random(params.h, o);
+
+        return params;
+    }
+
+    KeyPair generateKey(const Params &params, long index) {
+        KeyPair key;
+        key.privateKey.a = params.a;
+        key.publicKey.a = params.a;
+
+        NTL::ZZ_p::init(params.bound);
+
+        NTL::ZZ_pX f;
+        SetCoeff(f, params.k);
+        SetCoeff(f, 0);
+        NTL::ZZ_pE::init(f);
+
+        random(key.privateKey.s);
+
+        NTL::ZZ_pE e;
+        random(e);
+
+        NTL::ZZ_p::init(params.q);
+
+        key.publicKey.b = key.privateKey.a * key.privateKey.s + e;
+
+        return key;
+    }
+}
